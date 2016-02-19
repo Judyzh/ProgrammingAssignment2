d1c16c0d009a5fabf08324c3c53859fb6baa94cb
Judyzh <pearl_zhou@hotmail.com>
2016-02-19 17:57:27 +0800

    Update cachematrix.R
    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,13 @@
 
 ## Write a short comment describing this function
 
+## This function takes a matrix argument and return a list with
+## get and set matrix and get and set inverse matrix
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
-                x <<- y
+                x <<- y  
                 m <<- NULL
         }
         get <- function() x
@@ -17,12 +20,19 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
+## It is to check if inverse has been ready. If so, get inverse from cache
+## otherwise calculate inverse
+
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
+        
+        ## check if inversion is ready and return the data if so.
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        
+        ## otherwise, calculate inversion
         data <- x$get()
         m <- solve(data, ...)
         x$setinverse(m)
